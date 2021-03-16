adec1bd816a8630f63dd9486b4b32e8781cd2f8d
Hans Löfgren <mail@1two3code.com>
2021-03-16 15:10:07 +0100

    entered accountid

--- index.ts
+++ index.ts
@@ -1,17 +1,16 @@
 import cdk = require("@aws-cdk/core")
-import { StaticSiteStack } from "./stacks/s3-static-site-with-cloudfront"
+import { StaticSiteStack } from "./stacks/s3-cloudfront"
 
 const app = new cdk.App()
 const staticSite = new StaticSiteStack(app, "Dickachu", {
   env: {
-    account: '',
-    region: '',
+    account: '814967776290',
+    region: 'eu-north-1',
   },
   domainName: "dickachu.com",
   siteSubDomain: "yolo",
 })
 
-// example of adding a tag - please refer to AWS best practices for ideal usage
 cdk.Tags.of(staticSite).add("Dickachu", "Static Web")
 
 app.synth()
